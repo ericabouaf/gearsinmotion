f8c68999461ae931587afaf900dd37385a8dfbc4
Eric Abouaf <eric.abouaf@gmail.com>
2007-10-12 13:53:23 +0000

    Script that builds zip packages

--- script/go_standalone.bat
+++
@@ -1,4 +0,0 @@
-copy ..\js\helpers.js+..\js\gearsinmotion.js+..\js\gim_datatable_by_sql.js+..\js\sql.js tempo.js
-ruby jsmin.rb < tempo.js > ../gim_standalone.js
-erase tempo.js
-pause
--- script/go_standalone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-#
-#  This file generates a single "gim_standalone.js" file
-#
-cat ../js/helpers.js ../js/gearsinmotion.js ../js/gim_datatable_by_sql.js ../js/sql.js | ruby jsmin.rb > gim_standalone.js
