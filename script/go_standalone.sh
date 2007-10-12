a018f8fd9a5155f39f2edb9459a332d27c951a34
Eric Abouaf <eric.abouaf@gmail.com>
2007-10-12 12:36:08 +0000

    Cleanup the demo html. Comment in the standalone generation script. Translation of some french comments in CSS.

--- script/go_standalone.sh
+++ script/go_standalone.sh
@@ -1,3 +1,7 @@
-cat ../js/helpers.js ../js/gearsinmotion.js ../js/gim_datatable_by_sql.js ../js/sql.js > tempo.js
-ruby jsmin.rb < tempo.js > gim_standalone.js
-rm tempo.js
+#!/usr/bin/sh
+#
+#  This file generates a single "gim_standalone.js" file
+#
+cat ../js/helpers.js ../js/gearsinmotion.js ../js/gim_datatable_by_sql.js ../js/sql.js > temp.js
+ruby jsmin.rb < temp.js > gim_standalone.js
+rm temp.js
