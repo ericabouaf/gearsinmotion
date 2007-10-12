c70ac466d6b95879836461f989d4c87eacc8cd57
Eric Abouaf <eric.abouaf@gmail.com>
2007-10-12 12:55:59 +0000

    YUI 2.3.1, simplified (and working) script

--- script/go_standalone.sh
+++ script/go_standalone.sh
@@ -2,6 +2,4 @@
 #
 #  This file generates a single "gim_standalone.js" file
 #
-cat ../js/helpers.js ../js/gearsinmotion.js ../js/gim_datatable_by_sql.js ../js/sql.js > temp.js
-ruby jsmin.rb < temp.js > gim_standalone.js
-rm temp.js
+cat ../js/helpers.js ../js/gearsinmotion.js ../js/gim_datatable_by_sql.js ../js/sql.js | ruby jsmin.rb > gim_standalone.js
